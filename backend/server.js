1d570a4a7047d4f385afdbda3831773550de7f9a
Arif Billah <billaharif661@gmail.com>
2026-02-03 06:17:39 +0600

    new update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['https://landingpro.netlify.app', 'http://localhost:3000'],
+    origin: ['https://ephemeral-buttercream-eb339c.netlify.app', 'http://localhost:3000'],
     credentials: true
 }));
 app.use(express.json());
